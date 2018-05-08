bb4410000b40c89897562e08f64447fe4cefc8d2
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-08 17:30:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  dogs = [Max, Dougie, Penny, Betty]
+  dogs = ["Max", "Dougie", "Penny", "Betty"  ]
 end
 
 def add_element_to_end_of_array(array, element)
-  dogs << [Lana]
+  dogs << ["Lana"]
 end
 
 def add_element_to_start_of_array(array, element)
